05c71fba261f2b4e8b41d2310abf58cafcc0b26f
Frank Wolf <fwolf@posteo.de>
2020-02-15 12:32:36 +0100

    feat: add migrations, add completed column to db schema

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 import os, sys
+from flask_migrate import Migrate
 
-from app import settings
+import settings
 
 app = Flask(__name__)
 local = env_var = os.environ['LOCAL']
@@ -16,14 +17,15 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-db.create_all()
 
 @app.route("/todo/create", methods=['POST'])
 def create_todo():
